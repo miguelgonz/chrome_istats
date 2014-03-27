2d883acc44ddc1bd68ce636dbdca0da4873dbb43
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2014-03-27 14:29:36 +0000

    Initial working version

--- js/background.js
+++ js/background.js
@@ -7,14 +7,38 @@ function clear () {
     data = [];
 }
 
+var parseQueryString = function( queryString ) {
+    var params = {}, queries, temp, i, l;
+    // Split into key/value pairs
+    queries = queryString.split("&");
+    // Convert the array of strings into an object
+    for ( i = 0, l = queries.length; i < l; i++ ) {
+        temp = queries[i].split('=');
+        params[temp[0]] = temp[1];
+    }
+    return params;
+};
+
 function addDataListener (callback) {
     dataListener = callback;
 }
 
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(info) {
-      data.push(info);
-      dataListener();
+      var urlParts = info.url.split("?");
+      if (urlParts.length > 1) {
+          var params = parseQueryString(urlParts[1]);
+          for (var key in params) {
+              console.log(key);
+              if ( key === 'prev_page_type' ||
+                   key === 'page_type'
+              ) {
+                  data.push({'key':key, 'value':params[key]});
+              }
+          }
+          console.log(data);
+          dataListener();
+      }
   },
   { //filter
     urls: [ "http://sa.bbc.co.uk/*" ]
--- js/popup.js
+++ js/popup.js
@@ -1,22 +1,9 @@
-var parseQueryString = function( queryString ) {
-    var params = {}, queries, temp, i, l;
-    // Split into key/value pairs
-    queries = queryString.split("&");
-    // Convert the array of strings into an object
-    for ( i = 0, l = queries.length; i < l; i++ ) {
-        temp = queries[i].split('=');
-        params[temp[0]] = temp[1];
-    }
-    return params;
-};
-
 
 var refreshRequests = function () {
     for (var request in bg.data) {
-        var params = parseQueryString(bg.data[request].url);
-        for (var param in params) {
-            $('#content').append('<tr><td>'+param+'</td><td>'+params[param]+'</td></tr>');
-        }
+        var label = bg.data[request];
+        $('#content tbody').append(
+            '<tr><td>'+label.key+'</td><td>'+label.value+'</td></tr>');
     }
 };
 
